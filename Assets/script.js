9206c395da0caf5627b7978ce6a3faa017ddb5f0
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-07-20 09:35:56 +0200

    updimgurl

--- Assets/script.js
+++ Assets/script.js
@@ -7,37 +7,37 @@ let price = 10;
 
 let x = () => {
 if ( score >= 1000) {
-	document.getElementById("groot1").src="../Assets/img/guardiansgroup.png";
+	document.getElementById("groot1").src="Assets/img/guardiansgroup.png";
 } else if ( score >= 720) {
-	document.getElementById("groot1").src="../Assets/img/peterquill3.png";
+	document.getElementById("groot1").src="Assets/img/peterquill3.png";
 } else if ( score >= 650) {
-	document.getElementById("groot1").src="../Assets/img/peterquill2.png";
+	document.getElementById("groot1").src="Assets/img/peterquill2.png";
 } else if ( score >= 580) {
-	document.getElementById("groot1").src="../Assets/img/peterquill.png";
+	document.getElementById("groot1").src="Assets/img/peterquill.png";
 } else if ( score >= 510) {
-	document.getElementById("groot1").src="../Assets/img/gamora3.png";
+	document.getElementById("groot1").src="Assets/img/gamora3.png";
 } else if ( score >= 450) {
-	document.getElementById("groot1").src="../Assets/img/gamora2.png";
+	document.getElementById("groot1").src="Assets/img/gamora2.png";
 } else if ( score >= 390) {
-	document.getElementById("groot1").src="../Assets/img/gamora.png";
+	document.getElementById("groot1").src="Assets/img/gamora.png";
 } else if ( score >= 330) {
-	document.getElementById("groot1").src="../Assets/img/drax2.png";
+	document.getElementById("groot1").src="Assets/img/drax2.png";
 } else if ( score >= 280) {
-	document.getElementById("groot1").src="../Assets/img/drax1.png";
+	document.getElementById("groot1").src="Assets/img/drax1.png";
 } else if ( score >= 230) {
-	document.getElementById("groot1").src="../Assets/img/drax.png";
+	document.getElementById("groot1").src="Assets/img/drax.png";
 } else if ( score >= 180) {
-	document.getElementById("groot1").src="../Assets/img/rocket2.png";
+	document.getElementById("groot1").src="Assets/img/rocket2.png";
 } else if ( score >= 140) {
-	document.getElementById("groot1").src="../Assets/img/rocket1.png";
+	document.getElementById("groot1").src="Assets/img/rocket1.png";
 } else if ( score >= 100) {
-	document.getElementById("groot1").src="../Assets/img/rocket.png";
+	document.getElementById("groot1").src="Assets/img/rocket.png";
 } else if ( score >= 60) {
-	document.getElementById("groot1").src="../Assets/img/babygroot2.png";
+	document.getElementById("groot1").src="Assets/img/babygroot2.png";
 }  else if ( score >= 30) {
-	document.getElementById("groot1").src="../Assets/img/groot.png";
+	document.getElementById("groot1").src="Assets/img/groot.png";
 } else {
-	document.getElementById("groot1").src="../Assets/img/babygroot.png";
+	document.getElementById("groot1").src="Assets/img/babygroot.png";
 }}
 
 x();
